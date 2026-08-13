0553aae509010844b7a1d7f45e1920a2e10e064a
Manash Barman <manashbarman007@gmail.com>
2026-08-14 00:03:56 +0530

    Add import for List in DatabaseTool

--- spring-ai-mcp-server-stdio/src/main/java/in/sp/main/tools/DatabaseTool.java
+++ spring-ai-mcp-server-stdio/src/main/java/in/sp/main/tools/DatabaseTool.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import in.sp.main.entity.ShoppingCart;
 import in.sp.main.repo.ShoppingCartRepo;
+import java.util.List;
 
 @Service
 public class DatabaseTool {
